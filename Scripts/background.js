490963c50b73e6c51762048fde0111b26997158e
Dan Loewenherz <dloewenherz@gmail.com>
2015-02-06 09:54:36 -0600

    update info.plist

--- Scripts/background.js
+++ Scripts/background.js
@@ -1,5 +1,7 @@
 var urlRegex = /https?:\/\/github\.com\/([\w-]+)\/([\w-]+)\/(pulls|issues)(\??.*)/g;
 
+var DEBUG = safari.extension.settings.debug;
+
 function keyFromURL(url) {
     username = url.replace(urlRegex, "$1");
     repository = url.replace(urlRegex, "$2");
@@ -12,10 +14,18 @@ safari.application.addEventListener("message", function(event) {
     querystring = url.substring(pos);
 
     if (event.name === "save") {
+        if (DEBUG) {
+            console.log("Saving: " + url);
+        }
+
         querystring = url.replace(urlRegex, "$3$4");
         window.localStorage.setItem(keyFromURL(url), querystring);
     }
     else if (event.name === "replace") {
+        if (DEBUG) {
+            console.log("Replacing: " + url);
+        }
+
         querystring = window.localStorage.getItem(keyFromURL(url));
         if (querystring) {
             url = "https://github.com/" + keyFromURL(url) + "/" + querystring;
